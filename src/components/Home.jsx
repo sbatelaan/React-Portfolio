e585a9014389129cd0c60eb6ed854587fac004d2
stephen Batelaan <sbatelaan91@gmail.com>
2023-05-27 11:03:06 -0700

    responsive fix maybe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,15 +7,15 @@ const Home = () => {
   return (
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
 
-        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-            <div className=" max-w-4xl mx-auto flex flex-col justify-center h-full">
+        <div className="max-w-4xl mx-auto flex flex-col-reverse items-center justify-center h-full px-4 md:flex-row pt-40">
+            <div className=" mx-auto flex flex-col justify-center h-full">
                 <h2 className="text-4xl sm:text-7xl font-bold text-white">
                     Full Stack Web Developer
                 </h2>
                 <p className="text-gray-500 py-4 max-w-md">
                 Driven software engineer experienced in developing innovative and scalable solutions to complex challenges.
                 </p>
-                <div>
+                <div className='pb-40'>
                     <Link to="portfolio" smooth duration={500} className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-400 to-blue-500 cursor-pointer">
                     Portfolio
                         <span className="group-hover:rotate-90 duration-300">
@@ -24,7 +24,7 @@ const Home = () => {
                     </Link>
                 </div>
             </div>
-            <div className='pb-40'>
+            <div>
                 <img src={profilePic} alt="Stephen Batelaan" className="rounded-2xl mx-auto w-full md:w-2/3" />
             </div>
         </div>
